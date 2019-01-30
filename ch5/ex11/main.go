7ff8f6a2ffcd76b45a82a995f61988243774cfb6
韩剑 <hanjian@xgo.cn>
2019-01-30 19:08:27 +0800

    ch5

---
+++ ch5/ex11/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+)
+
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming", "data structures"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i, course)
+	}
+}
+
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items map[string]bool)
+	visitAll = func(items map[string]bool) {
+		for k, _ := range items {
+			if !seen[k] {
+				seen[k] = true
+				mm := make(map[string]bool, len(m[k]))
+				for _, kk := range m[k] {
+					mm[kk] = true
+					for _, s := range m[kk] {
+						if s == k {
+							fmt.Println("循环", kk, s)
+						}
+					}
+				}
+				visitAll(mm)
+				order = append(order, k)
+			}
+		}
+	}
+
+	keys := make(map[string]bool)
+	for k, _ := range m {
+		keys[k] = true
+	}
+	visitAll(keys)
+	return order
+}
---
+++ ch5/ex13/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gopl/ch5/links"
+)
+
+func main() {
+	breadthFirst(crawl, os.Args[1:])
+}
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func crawl(link string) []string {
+	fmt.Println(link)
+	list, err := links.Extract(link)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
